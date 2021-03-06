17cb6ac592fff1d243d2c88c92e91a6ccb2332b4
Adnrr <79010902+Adnrr@users.noreply.github.com>
2021-03-06 03:43:43 -0600

    Examen final

---
+++ Spawner/Assets/Script/colibre.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class colibre : MonoBehaviour
+{
+    public float speed;
+    public float distancia;
+    private Transform target;
+    
+    
+    void Start()
+    {
+        target= GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+    void Update()
+    {
+        if(Vector2.Distance(transform.position, target.position) > distancia)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position,speed * Time.deltaTime);
+        }
+    }
+
+
+
+
+}
---
+++ Spawner/Assets/Script/spawner.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawner : MonoBehaviour
+{
+    public float radio;
+    private float respawn;
+    public float timeRespawn;
+    public GameObject colibre;
+    public Transform Player;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player").transform;
+        respawn= 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, Player.position)<radio)
+        {
+            gameObject.GetComponent<Animator>().SetBool("Create", true);
+            if (timeRespawn <= 0)
+            {
+                Instantiate(colibre, transform.position, Quaternion.identity);
+                gameObject.GetComponent<Animator>().SetBool("Create", false);
+                respawn++;
+                timeRespawn = respawn;
+                if (respawn >= 5)
+                {
+                    respawn = 3;
+                        
+                }
+                else
+                {
+                    respawn = 0;
+                        
+                }
+
+            }
+            else
+            {
+                timeRespawn -= Time.deltaTime;
+
+            }
+        }
+        else
+        {
+            gameObject.GetComponent<Animator>().SetBool("Create", false);
+            respawn =0;
+        }
+       
+    }
+}
